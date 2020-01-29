4b9d395ae5f92ccc91d87c375421ef9cd85f6f87
Chris Mears <chris.mears@gmail.com>
2020-01-29 14:07:37 -0800

    feat: Split JSON into cities by state
    The reasoning here is that it will better facilitate PRs.

--- src/split-json.js
+++ src/split-json.js
@@ -4,11 +4,13 @@ import {
   readFile, existsSync, mkdirSync, writeFileSync, readFileSync,
 } from 'fs';
 import jsonFormat from 'json-format';
+import slugify from 'slugify';
 import { join } from 'path';
 import STATES from './states';
 
 const filePath = join(__dirname, '../data/breweries.json');
 const storePath = join(__dirname, '../data/us');
+const slugifyOptions = { remove: /[*+~.,()'"!:@]/g };
 
 readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
   if (!err) {
@@ -30,24 +32,42 @@ readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
         return;
       }
 
-      // Create empty file with empty array if it doesn't exist
-      const stateFilePath = `${storePath}/${state.slug}.json`;
-      if (!existsSync(stateFilePath)) {
-        writeFileSync(stateFilePath, '[]');
+      // Create empty folder for brewery state, if it doesn't exist
+      const statePath = `${storePath}/${state.slug}`;
+      if (!existsSync(statePath)) {
+        mkdirSync(statePath);
+      }
+
+      // Create brewery city file with empty array, if it doesn't exist
+      const citySlug = slugify(brewery.city.toLowerCase(), slugifyOptions);
+      const cityFilePath = `${statePath}/${citySlug}.json`;
+      if (!existsSync(cityFilePath)) {
+        writeFileSync(cityFilePath, '[]');
       }
 
       // Read file into variable
-      const stateBreweries = JSON.parse(readFileSync(stateFilePath));
+      const cityBreweries = JSON.parse(readFileSync(cityFilePath));
+
+      // Add tags property
+      // TODO: This should be an optional parameter
+      const updatedBrewery = brewery;
+      updatedBrewery.tags = [];
+
+      // Replace ID with brewery slug
+      updatedBrewery.id = slugify(updatedBrewery.name.toLowerCase(), slugifyOptions);
+
+      // Remove updated_at since this will be automatica
+      delete updatedBrewery.updated_at;
 
       // Append current brewery
-      stateBreweries.push(brewery);
+      cityBreweries.push(updatedBrewery);
 
       // Ovewrite file
       const jsonFormatConfig = {
         type: 'space',
         size: 2,
       };
-      writeFileSync(stateFilePath, jsonFormat(stateBreweries, jsonFormatConfig));
+      writeFileSync(cityFilePath, jsonFormat(cityBreweries, jsonFormatConfig));
     });
     console.log(`☹️ # of Errors: ${errors}`);
   } else {
