de56dc6708870666f945f13de33637deaef7c0f2
Chris Mears <chris.mears@gmail.com>
2020-11-02 20:27:34 -0800

    feat: Update split CSV task
    * Exclude / from slugs
    * Skip duplicate breweries

--- tasks/split-csv.js
+++ tasks/split-csv.js
@@ -7,13 +7,12 @@ import Papa from 'papaparse';
 import { join } from 'path';
 import slugify from 'slugify';
 
-const slugifyOptions = { remove: /[*+~.,()'"!:@]/g };
+const slugifyOptions = { remove: /[*+~.,()'"!:@/]/g };
 const csvFilePath = join(__dirname, '../breweries.csv');
 const storePath = join(__dirname, '../data');
 const headers = 'id,name,brewery_type,street,address_2,address_3,city,state,county_province,postal_code,website_url,phone,created_at,updated_at,country,longitude,latitude,tags';
 
-// TODO: Eventually, we want to skip duplicate IDs. But not today.
-// const skippedBreweries = [];
+const skippedBreweries = [];
 
 try {
   const csvFile = readFileSync(csvFilePath, { encoding: 'utf-8' });
@@ -57,27 +56,24 @@ try {
     const stateFile = readFileSync(stateFilePath, { encoding: 'utf-8' });
     const stateBreweries = Papa.parse(stateFile, { header: true });
 
-    // TODO: Eventually, we want to skip duplicate IDs. But not today.
-    // if (stateBreweries.data.find((b) => b.id === brewery.id)) {
-    //   console.log(`⏭ Skipping ${brewery.name}. (reason: duplicate)`);
-    //   skippedBreweries.push(brewery);
-    // } else {
-    // }
-
-    console.log(`✍️ Appending ${brewery.name} to ${stateFilePath}`);
-    stateBreweries.data.push(brewery);
+    if (stateBreweries.data.find((b) => b.id === brewery.id)) {
+      console.log(`⏭ Skipping ${brewery.name}. (reason: duplicate)`);
+      skippedBreweries.push(brewery);
+    } else {
+      console.log(`✍️ Adding ${brewery.name} to ${stateFilePath}`);
+      stateBreweries.data.push(brewery);
+    }
 
     writeFileSync(stateFilePath, Papa.unparse(stateBreweries.data));
   });
 
-  // TODO: Eventually, we want to skip duplicate IDs. But not today.
-  // if (skippedBreweries.length) {
-  //   const csvSkippedFilePath = join(__dirname, '../skipped.csv');
-  //   if (!existsSync(csvSkippedFilePath)) {
-  //     writeFileSync(csvSkippedFilePath, headers);
-  //   }
-  //   writeFileSync(csvSkippedFilePath, Papa.unparse(skippedBreweries));
-  // }
+  if (skippedBreweries.length) {
+    const csvSkippedFilePath = join(__dirname, '../skipped.csv');
+    if (!existsSync(csvSkippedFilePath)) {
+      writeFileSync(csvSkippedFilePath, headers);
+    }
+    writeFileSync(csvSkippedFilePath, Papa.unparse(skippedBreweries));
+  }
 
   console.log('✅ Success!');
 } catch (error) {
