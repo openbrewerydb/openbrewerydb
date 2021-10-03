efffa329d9c29835563b074cfb0fd67bca0ee848
Chris Mears <chris.mears@gmail.com>
2021-10-03 09:44:15 -0700

    feat: Update validation
    Properly use JSON Schema; Add uniqueness check

--- src/validate-csv.ts
+++ src/validate-csv.ts
@@ -1,11 +1,9 @@
-import { writeFileSync, readFileSync } from "fs";
+import { readFileSync } from "fs";
 import { join } from "path";
 import glob from "glob-promise";
 import type { Brewery } from "./utils/types";
-
-// @ts-ignore
-// csval is a newer library and doesn't have any published types yet
-import csval from "csval";
+import Papa from "papaparse";
+import Ajv from "ajv";
 
 const BREWERY_TYPES = [
   "micro",
@@ -21,53 +19,81 @@ const BREWERY_TYPES = [
   "closed",
 ];
 
-const main = async () => {
-  const fileGlob = join(__dirname, "../data/**/*.csv");
+const fileGlob = join(__dirname, "../data/**/*.csv");
 
-  const rules = {
-    $schema: "http://json-schema.org/schema#",
-    type: "object",
-    required: [
-      "id",
-      "name",
-      "brewery_type",
-      "city",
-      "country",
-    ],
-    properties: {
-      obdb_id: {
-        type: "string",
-      },
-      name: {
-        type: "string",
-      },
-      brewery_type: {
-        enum: BREWERY_TYPES,
-      },
-      street: {
-        type: "string",
-      },
-      city: {
-        type: "string",
-      },
-      country: {
-        type: "string",
-      },
+// JSON Schema Validation
+const ajv = new Ajv();
+const schema = {
+  type: "object",
+  required: ["obdb_id", "name", "brewery_type", "city", "country"],
+  properties: {
+    obdb_id: {
+      type: "string",
     },
-  };
+    name: {
+      type: "string",
+    },
+    brewery_type: {
+      enum: BREWERY_TYPES,
+    },
+    street: {
+      type: "string",
+    },
+    city: {
+      type: "string",
+    },
+    country: {
+      type: "string",
+    },
+  },
+};
+const validate = ajv.compile(schema);
 
+function checkValidity(data: Brewery) {
+  const valid = validate(data);
+  if (!valid) {
+    console.log(data);
+    console.error(validate.errors);
+    throw new Error("Invalid schema");
+  }
+}
+
+const ids: Record<string, Brewery> = {};
+function checkUniqueness(data: Brewery) {
+  if (ids[data.obdb_id]) {
+    console.log(ids[data.obdb_id]);
+    console.log(data);
+    throw new Error("ID is not unique");
+  }
+  ids[data.obdb_id] = data;
+}
+
+const main = async () => {
   try {
+    const startTime = new Date().getTime();
     const files = await glob(fileGlob);
+
     for (let file of files) {
       console.log(`📋 Validating ${file}...`);
-      const data = readFileSync(file, { encoding: "utf-8" });
-      const parsed: Brewery = await csval.parseCsv(data);
-      const valid = await csval.validate(parsed, rules);
+      const csv = readFileSync(file, { encoding: "utf-8" });
+      const breweries = await Papa.parse<Brewery>(csv, {
+        header: true,
+        skipEmptyLines: true,
+      });
+
+      for (let data of breweries.data) {
+        checkValidity(data);
+        checkUniqueness(data);
+      }
     }
-    console.log(`✅  All ${files.length} files are valid!`);
+
+    console.log(
+      `✅  All ${files.length} files are valid! (${
+        new Date().getTime() - startTime
+      }ms)`
+    );
   } catch (error) {
-    console.error(`${error}`);
-    throw new Error("🛑 Invalid CSV");
+    console.error(error);
   }
 };
 
