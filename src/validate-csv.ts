be22b046afb00925a3de6e98f0bf81d8068db9d4
Chris Mears <chris.mears@gmail.com>
2021-10-14 21:51:41 -0700

    Update validation (#100)
    * feat: Update validate schema; add breweries.csv
    
    * fix: Remove unnecessary await from Papa parse
    
    * fix: Throw error if catching an error to terminate script
    
    * fix: Handle UnhandledPromise
    
    * fix: Validate full dataset separately
    
    * fix: Fix washington.csv
    
    * chore: Combine; generate IDs; spit
    
    * chore: Generate SQL and JSON

--- src/validate-csv.ts
+++ src/validate-csv.ts
@@ -8,58 +8,68 @@ import { schema } from "./config";
 
 const ajv = new Ajv({ allowUnionTypes: true });
 const validate = ajv.compile(schema);
-const ids: Record<string, Brewery> = {};
 
-function checkValidity(data: Brewery) {
-  const valid = validate(data);
-  if (!valid) {
-    console.log(data);
-    console.error(validate.errors);
-    throw new Error("Invalid schema");
-  }
-}
+function validateFiles(files: string[]) {
+  const ids: Record<string, Brewery> = {};
 
-function checkUniqueness(data: Brewery) {
-  if (ids[data.obdb_id]) {
-    console.log(ids[data.obdb_id]);
-    console.log(data);
-    throw new Error("ID is not unique");
+  function checkValidity(data: Brewery) {
+    const valid = validate(data);
+    if (!valid) {
+      console.log(data);
+      console.error(validate.errors);
+      throw new Error("Invalid schema");
+    }
   }
-  ids[data.obdb_id] = data;
+
+  function checkUniqueness(data: Brewery) {
+    if (ids[data.obdb_id]) {
+      console.log(ids[data.obdb_id]);
+      console.log(data);
+      throw new Error("ID is not unique");
+    }
+    ids[data.obdb_id] = data;
+  }
+
+  for (let file of files) {
+    console.log(`📋 Validating ${file}...`);
+    const csv = readFileSync(file, { encoding: "utf-8" });
+    const breweries = Papa.parse<Brewery>(csv, {
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      transform: (value) => {
+        return value === "" ? null : value;
+      },
+    });
+
+    for (let data of breweries.data) {
+      checkValidity(data);
+      checkUniqueness(data);
+    }
+  }
 }
 
 const main = async () => {
-  try {
-    const startTime = new Date().getTime();
-    const fileGlob = join(__dirname, "../data/**/*.csv");
-    const files = await glob(fileGlob);
+  const startTime = new Date().getTime();
+  const fileGlob = join(__dirname, "../data/**/*.csv");
 
-    for (let file of files) {
-      console.log(`📋 Validating ${file}...`);
-      const csv = readFileSync(file, { encoding: "utf-8" });
-      const breweries = await Papa.parse<Brewery>(csv, {
-        header: true,
-        skipEmptyLines: true,
-        dynamicTyping: true,
-        transform: (value) => {
-          return value === "" ? null : value;
-        },
-      });
+  // Validate individual files
+  let files = await glob(fileGlob);
+  validateFiles(files);
 
-      for (let data of breweries.data) {
-        checkValidity(data);
-        checkUniqueness(data);
-      }
-    }
+  // Separately validate full dataset CSV
+  validateFiles([join(__dirname, "../breweries.csv")]);
 
-    console.log(
-      `✅  All ${files.length} files are valid! (${
-        new Date().getTime() - startTime
-      }ms)`
-    );
-  } catch (error) {
-    console.error(error);
-  }
+  console.log(
+    `✅  All ${files.length + 1} files are valid! (${
+      new Date().getTime() - startTime
+    }ms)`
+  );
 };
 
-main();
+try {
+  main();
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
