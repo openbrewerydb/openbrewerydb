7844145ab1e917cc9cf251e92548d2e4d745fbfa
Jerry Tong <zihangtong@gmail.com>
2021-12-07 11:13:13 -0500

    added submit new brewery

--- src/index.js
+++ src/index.js
@@ -10,10 +10,59 @@ document.addEventListener('DOMContentLoaded', (e) => {
     getBrewData();
 })
 
+const breweryURL = 'https://api.openbrewerydb.org/breweries';
+
+const newBreweryForm = document.querySelector('#new-brewery-form');
+const newName = document.querySelector('#new-name');
+const newBreweryType = document.querySelector('#new-brewery-type');
+const newStreet = document.querySelector('#new-street');
+const newCity = document.querySelector('#new-city');
+const newState = document.querySelector('#new-state');
+const newPostal = document.querySelector('#new-postal-code');
+const newCountry = document.querySelector('#new-country');
+const newPhone = document.querySelector('#new-phone');
+const newWebsite = document.querySelector('#new-website-url');
+
 function getBrewData() {
-    fetch('https://api.openbrewerydb.org/breweries')
+    fetch(breweryURL)
     .then(res => res.json())
     .then(data => {
         console.log(data);
     })
+}
+
+function postBrewData(breweryObj) {
+    const postObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(breweryObj)
+    }
+    fetch(breweryURL, postObj)
+    .then(res => res.json())
+    .then(newBrewery => {
+        console.log(newBrewery);
+    })
+}
+
+// Event Listeners
+newBreweryForm.addEventListener('submit', handleSubmit)
+
+// Event Handlers
+function handleSubmit(e) {
+    e.preventDefault();
+    const breweryObj = {
+        name: newName.value,
+        brewery_type: newBreweryType.value,
+        street: newStreet.value,
+        city: newCity.value,
+        state: newState.value,
+        postal_code: newPostal.value,
+        country: newCountry.value,
+        phone: newPhone.value,
+        website_url: newWebsite.value,
+    };
+    postBrewData(breweryObj);
+    newBreweryForm.reset();
 }
