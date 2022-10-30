3e1417f96459a425bfdeb9fe0ae233d5e6dded98
Chris J Mears <chris.mears@gmail.com>
2022-10-29 22:15:25 -0700

    Remove breweries-lite
    It was fairly pointless.

--- src/combine-csv.ts
+++ src/combine-csv.ts
@@ -4,16 +4,14 @@ import { writeFileSync, readFileSync } from "fs";
 import { join } from "path";
 import glob from "glob";
 import Papa from "papaparse";
-import { headers, headersLite } from "./config";
-import { Brewery, BreweryLite } from "./utils/types";
+import { headers } from "./config";
+import { Brewery } from "./utils/types";
 
 const fileGlob = join(__dirname, "../data/**/*.csv");
 const fullFilePath = join(__dirname, "../breweries.csv");
-const liteFilePath = join(__dirname, "../breweries-lite.csv");
 
 glob(fileGlob, {}, (globError, files) => {
   const breweries: Brewery[] = [];
-  const breweriesLite: BreweryLite[] = [];
 
   if (!globError) {
     files.forEach((file) => {
@@ -36,19 +34,6 @@ glob(fileGlob, {}, (globError, files) => {
         // Full dataset
         const dataFull = result.data;
         breweries.push(...dataFull);
-
-        // Go through each data object and create new lite version
-        const dataLite: BreweryLite[] = result.data
-          .filter((brewery) => brewery.brewery_type !== "closed")
-          .map((brewery) => {
-            return {
-              name: brewery.name,
-              city: brewery.city,
-              state: brewery.state,
-              country: brewery.country,
-            };
-          });
-        breweriesLite.push(...dataLite);
       } catch (error) {
         console.error(error);
       }
@@ -57,7 +42,6 @@ glob(fileGlob, {}, (globError, files) => {
     if (breweries.length) {
       // Sort breweries by ID
       breweries.sort((a, b) => a.obdb_id.localeCompare(b.obdb_id));
-      breweriesLite.sort((a, b) => a.name.localeCompare(b.name));
 
       console.log(
         `Writing full dataset to ${fullFilePath} (${breweries.length} breweries)`
@@ -66,17 +50,6 @@ glob(fileGlob, {}, (globError, files) => {
         fullFilePath,
         Papa.unparse(breweries, {
           columns: headers,
-          skipEmptyLines: true,
-        })
-      );
-
-      console.log(
-        `Writing lite dataset to ${liteFilePath} (${breweriesLite.length} breweries)`
-      );
-      writeFileSync(
-        liteFilePath,
-        Papa.unparse(breweriesLite, {
-          columns: headersLite,
           skipEmptyLines: true,
         })
       );
