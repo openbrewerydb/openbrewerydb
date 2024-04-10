1a5be9009b2e30f7ca7a5b1fd16d2ff5464aa6c7
Chris Mears <chris.mears@gmail.com>
2024-04-10 12:16:27 -0500

    Add generator for brewries map JSON

---
+++ src/generate-map-json.ts
@@ -0,0 +1,40 @@
+import { writeFileSync, readFileSync } from "fs";
+import { join } from "path";
+import Papa from "papaparse";
+import { papaParseOptions } from "./config";
+import { Brewery } from "./types";
+
+const csvFilePath = join(__dirname, "../breweries.csv");
+const jsonFilePath = join(__dirname, "../breweries-map.json");
+
+interface BreweryMap {
+  name: string;
+  lngLat: number[];
+  id: string;
+}
+
+try {
+  const data = readFileSync(csvFilePath, { encoding: "utf-8" });
+  const result = Papa.parse<Brewery>(data, papaParseOptions);
+  let breweries: BreweryMap[] = [];
+
+  for (let brewery of result.data) {
+    if (brewery.latitude && brewery.longitude) {
+      breweries.push({
+        id: brewery.id,
+        name: brewery.name,
+        lngLat: [brewery.longitude, brewery.latitude],
+      })
+    }
+  }
+
+  if (breweries.length) {
+    console.log(`📝 Writing to ${jsonFilePath}`);
+    writeFileSync(jsonFilePath, JSON.stringify(breweries));
+
+    console.log("Summary:");
+    console.log(`🍺 Total Breweries: ${breweries.length}`);
+  }
+} catch (error) {
+  console.error(`🛑 ${error}`);
+}
